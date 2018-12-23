2b2f2ae1c969247ce8f32608ca7921fc2fb3f0fd
saifulislamrajon <saifulrajon87@gmail.com>
2018-12-23 22:08:46 +0600

    23-12-2018 push 1

--- app/src/main/java/com/example/saiful/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/saiful/musicplayer/MainActivity.java
@@ -8,9 +8,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -22,11 +26,16 @@ public class MainActivity extends AppCompatActivity {
     String[] item;
     final int REQUEST_IMAGE_CALL = 1;
     ArrayList<File> mySongs;
+    Toolbar toolbar;
+    ImageButton mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
         ivPlaylist = (ListView) findViewById(R.id.lvPlaylist);
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -46,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         item = new String[mySongs.size()];
         for (int i = 0; i < mySongs.size(); i++) {
             //Toast.makeText(getApplicationContext(),mySongs.get(i).getName().toString(),Toast.LENGTH_SHORT).show();
-            item[i] = mySongs.get(i).getName().toString().replace(",mp3", "");
+            item[i] = mySongs.get(i).getName().toString().replace(".mp3", "");
+//            Toast.makeText(getApplicationContext(),item[i],Toast.LENGTH_LONG).show();
         }
 
         ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplicationContext(), R.layout.song_layout, R.id.textView, item);
@@ -58,6 +68,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Player.class).putExtra("pos", position).putExtra("songlist", mySongs));
             }
         });
+
+
+        /*mButton= (ImageButton) findViewById(R.id.mButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Player.class));
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(MainActivity.this, "it's item one", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(MainActivity.this, "it's item two", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     public ArrayList<File> findsong(File root) {
--- app/src/main/java/com/example/saiful/musicplayer/Player.java
+++ app/src/main/java/com/example/saiful/musicplayer/Player.java
@@ -3,12 +3,20 @@ package com.example.saiful.musicplayer;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,12 +35,24 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
     Button btPlay, btFF, btNxt, btFV, btPv;
     TextView show;
     EditText editText;
+    int pos = 0;
+//    It's searchView Work
+    SearchView searchView;
+    ListView listView2;
+    ArrayAdapter<String> adapter;
+    String[] itm;
+    Toolbar toolbar;
+    ImageButton pButton;
+
+//    AutoCompleteTextView autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-
+        toolbar= (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
         btPlay = (Button) findViewById(R.id.btPlay);
         btFF = (Button) findViewById(R.id.btFF);
         btNxt = (Button) findViewById(R.id.btNxt);
@@ -46,16 +66,50 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
         btPv.setOnClickListener(this);
 
         show = (TextView) findViewById(R.id.show);
-        editText = (EditText) findViewById(R.id.editText);
+
+        /*autoCompleteTextView= (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,mySongs);
+        autoCompleteTextView.setAdapter(adapter);*/
+        /*editText = (EditText) findViewById(R.id.editText);
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == event.ACTION_DOWN) && (keyCode == event.KEYCODE_ENTER)) {
-                    Toast.makeText(getApplicationContext(),"it's demo talk",Toast.LENGTH_SHORT).show();
+                    String songName = editText.getText().toString().toLowerCase();
+//                    Toast.makeText(getApplicationContext(),"Under Construction",Toast.LENGTH_SHORT).show();
+                    for (File f : mySongs) {
+                        String fName = f.getName().toLowerCase().replace(".mp3", "");
+                        pos += 1;
+                        if (fName.equals(songName)) {
+                            if (mp != null) {
+                                mp.stop();
+                                mp.release();
+                            }
+                            position = 0;
+                            uri = Uri.parse(mySongs.get(position = pos - 1).toString());
+                            show.setText(mySongs.get(position = pos - 1).toString());
+                            mp = MediaPlayer.create(getApplicationContext(), uri);
+                            mp.start();
+
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Player.this);
+                            alertDialogBuilder.setMessage(fName);
+                            AlertDialog alertDialog = alertDialogBuilder.create();
+                            alertDialog.show();
+//                            Toast.makeText(getApplicationContext(),"it's true",Toast.LENGTH_SHORT).show();
+
+                            break;
+                        }
+//                        Toast.makeText(getApplicationContext(), " " + pos, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(getApplicationContext(),f.getName(),Toast.LENGTH_SHORT).show();
+//                        pos+=1;
+                    }
+                    ////
+                    return true;
                 }
                 return false;
             }
-        });
+        });*/
+
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         updateSeekBar = new Thread() {
             @Override
@@ -67,7 +121,12 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
                 while (currentPosition < totalDuration) {
                     try {
                         sleep(500);
-                        currentPosition = mp.getCurrentPosition();
+                        try {
+                            currentPosition = mp.getCurrentPosition();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
                         seekBar.setProgress(currentPosition);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -106,6 +165,62 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 mp.seekTo(seekBar.getProgress());
 
+            }
+        });
+//        It's searchView work
+        itm=new String[mySongs.size()];
+        for(int j=0;j<mySongs.size();j++){
+            itm[j]=mySongs.get(j).getName().toString();
+        }
+        searchView= (SearchView) findViewById(R.id.searchView);
+        listView2= (ListView) findViewById(R.id.listView2);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itm);
+        listView2.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int positionn, long id) {
+                Toast.makeText(Player.this, " "+parent.getItemAtPosition(positionn)+" "+positionn, Toast.LENGTH_SHORT).show();
+                String uName= (String) parent.getItemAtPosition(positionn);
+                for(File f:mySongs){
+                    String sName=f.getName();
+                    pos += 1;
+                    if(sName.equals(uName)){
+                        if (mp != null) {
+                            mp.stop();
+                            mp.release();
+                        }
+                        try{
+                            position = 0;
+                            uri = Uri.parse(mySongs.get(position = pos - 1).toString());
+                            show.setText(mySongs.get(position = pos - 1).toString());
+                            mp = MediaPlayer.create(getApplicationContext(), uri);
+                            mp.start();
+                        }catch (IndexOutOfBoundsException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+
+        });
+        pButton= (ImageButton) findViewById(R.id.pButton);
+        pButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Player.this,MainActivity.class));
             }
         });
     }
@@ -152,7 +267,8 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
-    public void search(View view) {
+    /*public void search(View view) {
+        *//*Toast.makeText(Player.this, "it's search option", Toast.LENGTH_SHORT).show();
         String search_song = editText.getText().toString();
         while (mySongs.equals(search_song)) {
             uri = Uri.parse(mySongs.get(position).toString());
@@ -160,6 +276,35 @@ public class Player extends AppCompatActivity implements View.OnClickListener {
             mp = MediaPlayer.create(getApplicationContext(), uri);
             mp.start();
 
+        }*//*
+        String songName = editText.getText().toString().toLowerCase();
+//                    Toast.makeText(getApplicationContext(),"Under Construction",Toast.LENGTH_SHORT).show();
+        for (File f : mySongs) {
+            String fName = f.getName().toLowerCase().replace(".mp3", "");
+            pos += 1;
+            if (fName.equals(songName)) {
+                if (mp != null) {
+                    mp.stop();
+                    mp.release();
+                }
+                position = 0;
+
+                uri = Uri.parse(mySongs.get(position = pos - 1).toString());
+                show.setText(mySongs.get(position = pos - 1).toString());
+                mp = MediaPlayer.create(getApplicationContext(), uri);
+                mp.start();
+
+                            *//*AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Player.this);
+                            alertDialogBuilder.setMessage(fName);
+                            AlertDialog alertDialog = alertDialogBuilder.create();
+                            alertDialog.show();*//*
+//                            Toast.makeText(getApplicationContext(),"it's true",Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+//            Toast.makeText(getApplicationContext(), " " + pos, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(getApplicationContext(),f.getName(),Toast.LENGTH_SHORT).show();
+//                        pos+=1;
         }
-    }
+    }*/
 }
